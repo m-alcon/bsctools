134258ae25856782a2681459cb299db65ff1bb17
miquelalcon <miquel.1996@gmail.com>
2019-04-17 14:47:39 +0200

    MOD build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ if [ "$1" = "arm" ]; then
     docker pull miquelalcon/bsctools:arm64v8
 else
     docker pull miquelalcon/bsctools:latest #x86_64
+fi
 
 docker run -dit                         \
     --privileged                        \
